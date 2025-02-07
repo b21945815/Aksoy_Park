93d3239a037005b239ba37c7c1112887b74a7b9f
fatih656795 <f.ay@tryoto.com>
2025-02-07 09:44:37 +0300

    V2.3

--- src/ui/MobileHeader.jsx
+++ src/ui/MobileHeader.jsx
@@ -151,6 +151,7 @@ function MobileHeader() {
           <FiChevronLeft size={30} color="var(--color-grey-900)" />
         </BackButton>
         <MenuLink to="/works" onClick={toggleMenu}>Çalışmalarımız</MenuLink>
+        <MenuLink to="/examples" onClick={toggleMenu}>Örnekler</MenuLink>
         <MenuLink to="/information" onClick={toggleMenu}>Kurumsal</MenuLink>
         <MenuLink to="#" onClick={openProducts}>Ürünler</MenuLink>
       </MenuContainer>

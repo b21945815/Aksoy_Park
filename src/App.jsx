d3a435b6314b3877ceeea9235ca0e1dbbf9b794b
fatih656795 <f.ay@tryoto.com>
2025-02-22 20:34:35 +0300

    V3.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
+import { Suspense, lazy } from 'react'
 
 import GlobalStyles from './styles/GlobalStyles'
-import Information from './pages/Information'
-import Admin from './pages/Admin'
-import Login from './pages/Login'
-import PageNotFound from './pages/PageNotFound'
 import AppLayout from './ui/AppLayout'
-import Works from './pages/Works'
-import Examples from './pages/Examples'
-import ChildrenParkProduct from './pages/ChildrenParkProduct'
 import { DarkModeProvider } from './context/DarkModeContext'
+
+// Lazy-loaded pages
+const Information = lazy(() => import('./pages/Information'))
+const Admin = lazy(() => import('./pages/Admin'))
+const Login = lazy(() => import('./pages/Login'))
+const PageNotFound = lazy(() => import('./pages/PageNotFound'))
+const Works = lazy(() => import('./pages/Works'))
+const Examples = lazy(() => import('./pages/Examples'))
+const ChildrenParkProduct = lazy(() => import('./pages/ChildrenParkProduct'))
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -27,21 +31,23 @@ function App () {
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to='information' />} />
-              <Route path='information' element={<Information />} />
-              <Route path='works' element={<Works />} />
-              <Route path='examples' element={<Examples />} />
-              <Route path='admin' element={<Admin />} />
-              <Route path="/childrenParkProduct/:categoryName/:itemName" element={<ChildrenParkProduct />} />
-            </Route>
+        <HashRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route index element={<Navigate replace to='information' />} />
+                <Route path='information' element={<Information />} />
+                <Route path='works' element={<Works />} />
+                <Route path='examples' element={<Examples />} />
+                <Route path='admin' element={<Admin />} />
+                <Route path='/childrenParkProduct/:categoryName/:itemName' element={<ChildrenParkProduct />} />
+              </Route>
 
-            <Route path='login' element={<Login />} />
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+              <Route path='login' element={<Login />} />
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </HashRouter>
         <Toaster
           position='top-center'
           gutter={12}
